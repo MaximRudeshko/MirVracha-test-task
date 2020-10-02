2acfcbecb19e3115291fe315b95ac6449f340ae7
Maxim <Rudeshkomaximsergeevich@gmail.com>
2020-10-02 07:54:49 +0300

    added production build

---
+++ build/precache-manifest.80beb8fec6867182a419286ea3051946.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c3ede34ae0037179a5b06c7c8dea002",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de26511f021d22c04099",
+    "url": "/static/css/main.057626a0.chunk.css"
+  },
+  {
+    "revision": "835c49e654fcb44fb02c",
+    "url": "/static/js/2.87ac1892.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/static/js/2.87ac1892.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de26511f021d22c04099",
+    "url": "/static/js/main.d4e4724e.chunk.js"
+  },
+  {
+    "revision": "4d54752767e7bea57541",
+    "url": "/static/js/runtime-main.3f5a40dc.js"
+  },
+  {
+    "revision": "9d219696d0cb0e713b5dd38ab5c8c999",
+    "url": "/static/media/not-found.9d219696.svg"
+  }
+]);
